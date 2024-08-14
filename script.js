862c76364b9a8f8a4a4c2307ad25072f441b3026
Munaraaz <munmk@bk.ru>
2024-08-14 03:13:45 +0300

    hw-21 finish

---
+++ script.js
@@ -0,0 +1,66 @@
+//! Task 1
+
+// const student1 = {
+//     name: "John",
+//     lang: "KG",
+//     age: 24
+// };
+// const student2 = student1
+
+// function students(
+//     student1,student2
+// ){
+//    if(student1.length===student2.length) {delete student2.age}
+// };
+// students ( student1,student2);
+//  console.log(student2);
+
+
+
+//! Task 2
+// const array = [
+//     1,
+//     true, 
+//     3,
+//     { name: "ZH", hobbies: ["swimming", "reading"] },
+//     5,
+//     "hello",
+//     "hi",
+// ];
+// console.log(array);
+// let array1 = array [1]; 
+// console.log(array1);
+// let array2 = array [4];
+// console.log(array2);
+// let array3 = array[3].hobbies;
+// console.log(array3);
+// let array4 = array [3].hobbies.shift();
+// console.log(array4);
+// array.unshift('laptop');
+// console.log(array);
+// array.unshift('peaksoft');
+// console.log(array);
+// array.shift(2);
+// console.log(array);
+// let array5 = array.indexOf("hello");
+// console.log(array5);
+// let array6 = array.slice(0, 3);
+// console.log(array6);
+// array[3].hobbies=['swimming','reading','football'];
+// console.log(array);
+// array.splice(5,'hi');
+// console.log(array);
+
+//! Task 4
+
+// const arrays = [1, 2, 3, "sdf", 4, 'sdf', {}, 3, 4, true, 2, [1, 6, 3], 1];
+
+// let sum = arrays.reduce((prevVal, currVal, index)=> {
+//         if (typeof prevVal =="number" && typeof currVal =="number"){
+//          return currVal+prevVal;
+//         } else {
+//           return prevVal;
+//        }
+//        });
+//     console.log(sum);
+   
