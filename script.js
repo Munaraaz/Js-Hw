c2609cb4f529654311e60b417705d1381fe6b82d
Munaraaz <munmk@bk.ru>
2024-08-14 03:21:02 +0300

    hw-22

---
+++ script.js
@@ -0,0 +1,84 @@
+//! Task 1
+// const numbers = [1, 2, 3, 4, 5];
+
+// let num = numbers.map((element) => element **2);
+// console.log(num);
+
+//! Task 2
+
+// const users = [
+//   { firstName: "John", lastName: "Obama", age: 34, gender: "male" },
+//   { firstName: "Ariana", lastName: "Grande", age: 22, gender: "female" },
+//   { firstName: "Naruto", lastName: "Uzumaki", age: 19, gender: "male" },
+//   { firstName: "Cristiano", lastName: "Ronaldo", age: 38, gender: "male" },
+// ];
+
+// let array = users.map((element) => {
+//   return {
+//     fullName: element.firstName + " " + element.lastName,
+//     age: element.age,
+//     gender: element.gender,
+//   };
+// });
+// console.log(array);
+
+//! Task 3
+// const numbers1 = [1, 2, 3, 4, 5, 4, 8, 34, 3, 6];
+
+// let num1 =numbers1.filter((item) =>   item % 2 === 0);
+// console.log(num1);
+
+
+
+//! Task 4 
+// const users = [
+//   { firstName: "John", lastName: "Obama", age: 34, gender: "male" },
+//   { firstName: "Ariana", lastName: "Grande", age: 18, gender: "female" },
+//   { firstName: "Naruto", lastName: "Uzumaki", age: 19, gender: "male" },
+//   { firstName: "Cristiano", lastName: "Ronaldo", age: 28, gender: "male" },
+//   { firstName: "Sakura", lastName: "Chan", age: 18, gender: "female" },
+// ];
+// let gender = users.filter((element )=> element.gender === "female")
+// console.log(gender);
+//! Task 5
+
+// const numbers2 = [1, 2, 3, 4, 5, 4, 8, 34, 3, 6];
+// let sum = numbers2.reduce ((a, b) => a + b);
+// console.log(sum);
+
+
+
+//! Task 6
+
+// const numbers2 = [1, 2, 3, 4, 5, 4, 8, 34, 3, 6];
+
+// let sum = numbers2.reduce((a, b ) => {return a+b})
+// console.log(sum);
+
+
+//! Task 6 
+
+// const numbers3 = [1, 2, 3, 4, 5, 4, 8, 34, 3, 6];
+
+
+//! Task 7
+
+// const users = [
+//   { firstName: "John", lastName: "Obama", age: 34, gender: "male" },
+//   { firstName: "Ariana", lastName: "Grande", age: 18, gender: "female" },
+//   { firstName: "Naruto", lastName: "Uzumaki", age: 19, gender: "male" },
+//   { firstName: "Cristiano", lastName: "Ronaldo", age: 28, gender: "male" },
+//   { firstName: "Sakura", lastName: "Chan", age: 18, gender: "female" },
+// ];
+
+// let firstName = users.find((element) => element.firstName ==="Naruto")
+// console.log(firstName);
+
+
+//! Task 8
+
+// const numbers4 = [1, 2, 3, 4, 5, 4, 8, 34, 3, 6];
+
+// const User4 = numbers4.findIndex((item) => item === 34);
+
+// console.log(User4);
